5d864dc164bc354291e0baf2116ba4a93d058df6
Taake <trollfrost666@gmail.com>
2023-03-16 17:34:04 +0300

    Initial commit

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,53 +1,29 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
 
 class ChromeTest {
 
-    private WebDriver driver;
 
-    @BeforeAll
-
-    static void setUpAll() {
-
-        WebDriverManager.chromedriver().setup();
-
-    }
-
-
-    @BeforeEach
-    void setup() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
 
     void shouldTest() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Иванов-Смирнов Иван");
-        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+79119999999");
-        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button__text")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("p[data-test-id=order-success]")).getText().trim();
-        Assertions.assertEquals(expected, actual);
 
+        Configuration.holdBrowserOpen = true;
 
+        open ("http://localhost:9999/");
+        $("span[data-test-id=city] input").setValue("Казань");
+        $("span[data-test-id=date] input").setValue("19.03.2023");
+        $("span[data-test-id=name] input").setValue("Смирнов-Иванов Петр");
+        $("span[data-test-id=phone] input").setValue("+79119999999");
+        $("label[data-test-id=agreement]").click();
+        $x("//span[contains(text(), 'Забронировать')]").click();
+        $("div[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
 
     }
 
